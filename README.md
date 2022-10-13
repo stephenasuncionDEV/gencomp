f341fa2dfd614e1f92112d28cef0daa94719c5ce
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-12 22:12:22 -0700

    🔥 REMOVE: removed setRoot

--- README.md
+++ README.md
@@ -4,6 +4,20 @@
 
 GenComp automatically generates new component from selected code.
 
+## Install
+
+Install through VS Code extensions. Search for `gencomp`
+
+Can also be installed in VS Code: Launch VS Code Quick Open (Ctrl+P), paste the following command, and press enter.
+
+```
+ext install StephenAsuncion.gencomp
+```
+
+## Setup
+
+
+
 ## License
 
 [MIT](https://github.com/stephenasuncionDEV/gencomp/blob/main/LICENSE)
--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,10 @@ import { setConfig, generate } from "./commands";
 
 export function activate(context: ExtensionContext) {
 
-	const setRootFunc = commands.registerCommand('gencomp.setRoot', () => setConfig('rootPath'));
 	const setOutputFunc = commands.registerCommand('gencomp.setOutput', () => setConfig('outputPath'));
 	const generateFunc = commands.registerCommand('gencomp.generate', generate);
 
 	context.subscriptions.push(
-		setRootFunc,
 		setOutputFunc,
 		generateFunc
 	);
